8e9764e304ca454d2bf7cb0a1da758bc90cfc9bd
K1CKBOXER <120112541+K1CKBOXER@users.noreply.github.com>
2023-02-01 12:45:53 +0300

    Update device.cpp

--- device.cpp
+++ device.cpp
@@ -3,8 +3,15 @@
 #include "string"
 #include "IElectronics.h"
 #include "device.h"
+#include "appliances.h"
 
-Device::Device(int battery) : _battery(battery)
+using namespace std;
+
+Device::Device(int battery, string clr, string size) : _battery(battery), _clr(clr), _size(size)
+{
+}
+
+Device::Device(string clr)
 {
 }
 
@@ -15,11 +22,29 @@ void Device::Show()
 
 
 
-Player::Player(int battery, int totalTracks) : Device(battery), _totalTracks(totalTracks)
+Player::Player(int battery, int totalTracks, double weight, string material, int warranty, string clr, string size) : Device(battery, clr, size), Appliances(weight, warranty, material), _totalTracks(totalTracks)
 {
 }
 
 void Player::Show()
 {
-	cout << "Емкость батареи: " << _battery << "мА*ч" << endl << "Количество треков, которые можно записать: " << _totalTracks << endl;
+	cout << "Емкость батареи: " << _battery << " мА*ч" << endl << "Можно записать треков: " << _totalTracks << endl << "Материал: " << _material << endl << "Вес: " << _weight << " грамм" << endl << "Цвет: " << _clr << endl << "Размер(высота/глубина/ширина) в мм: " << _size << endl << "Гарантия: " << _warranty << " г." << endl << endl;
+}
+
+Smartphone::Smartphone(int battery, string material, string clr, double weight, string size, int memory, int warranty) : Device(battery, clr, size), Appliances(weight, warranty, material), _memory(memory)
+{
+}
+
+void Smartphone::Show()
+{
+	cout << "Емкость батареи: " << _battery << " мА*ч" << endl << "Материал: " << _material << endl << "Вес: " << _weight << " грамм" << endl << "Размеры (в/г/ш) в мм: " << _size << endl << "Количество памяти: " << _memory << " Гб" << endl << "Гарантия: " << _warranty << " г." << endl << endl;
+}
+
+Speaker::Speaker(int battery, string material, string clr, double weight, string size, int volume, int warranty) : Device(battery, clr, size), Appliances(weight, warranty, material), _volume(volume)
+{
+}
+
+void Speaker::Show()
+{
+	cout << "Емкость батареи: " << _battery << " мА*ч" << endl << "Материал: " << _material << endl << "Вес: " << _weight << " грамм" << endl << "Размеры (в/г/ш) в мм: " << _size << endl << "Громкость: " << _volume << " ДБ" << endl << "Гарантия: " << _warranty << " г." << endl << endl;
 }
