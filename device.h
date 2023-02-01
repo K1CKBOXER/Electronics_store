6c9eb0ef69dc67964390be800c14b61aab7e38d5
K1CKBOXER <120112541+K1CKBOXER@users.noreply.github.com>
2023-02-01 12:48:54 +0300

    Update device.h

--- device.h
+++ device.h
@@ -2,24 +2,46 @@
 #include <iostream>
 #include "string"
 #include "IElectronics.h"
+#include "appliances.h"
 
 using namespace std;
 
 class Device : virtual public IElectronics
 {
 public:
-	Device(int battery);
+	Device(int battery, string clr, string size);
+	Device(string clr);
 	virtual void Show() override;
 protected:
 	int _battery;
+	string _clr;
+	string _size;
 };
 
-class Player : virtual public Device
+class Player : public Device, Appliances
 {
 public:
-	Player(int battery, int totalTracks);
-	virtual void Show() override;	
+	Player(int battery, int totalTracks, double weight, string material, int warranty, string clr, string size);
+	void Show() override;	
 
 private:
 	int _totalTracks;
+};
+
+class Smartphone : public Device, Appliances
+{
+public:
+	Smartphone(int battery, string material, string clr, double weight, string size, int memory, int warranty);
+	void Show()override;
+private:
+	int _memory;
+};
+
+class Speaker : public Device, Appliances
+{
+public:
+	Speaker(int battery, string material, string clr, double weight, string size, int volume, int warranty);
+	void Show() override;
+private:
+	int _volume;
 };
